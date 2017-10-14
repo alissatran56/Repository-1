0ed163366268be48d04335412cdcc43e71da5e22
Xuan-Tien <Xuan-Tien@Mommy_Pavillion.Home>
2017-10-14 14:23:35 -0600

    comitting main

---
+++ src/program/Main.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package program;
+
+/**
+ * @author Xuan-Tien
+ *
+ */
+public class Main
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+
+	}
+
+}
